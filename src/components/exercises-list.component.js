fd1eb5415f2ca80962af2300b6d8990290d5eab8
jessebettridge <jessebett92@gmail.com>
2019-08-20 18:43:02 -0600

    made front-end changes

---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ExercisesLists extends Component {
+  render() {
+    return (
+      <div>
+        <p>You are on the Exercises list</p>
+      </div>
+    );
+  }
+}
